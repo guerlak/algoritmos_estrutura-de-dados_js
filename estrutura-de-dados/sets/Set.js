18812c1cf9cf3a75a029a724f8f6c73a1c2d247b
Rafael Guerlak <guerlak@hotmail.com>
2020-06-29 13:58:30 -0300

    sets

---
+++ estrutura-de-dados/sets/Set.js
@@ -0,0 +1,40 @@
+class Set {
+  constructor() {
+    this.items = {};
+  }
+
+  add(element) {
+    if (this.has(element)) {
+      return false;
+    }
+    this.items[element] = element;
+    return true;
+  }
+
+  delete(element) {
+    if (!this.has(element)) {
+      return false;
+    }
+    delete this.items[element];
+    return true;
+  }
+
+  has(element) {
+    // return element in this.items;
+    return Object.prototype.hasOwnProperty.call(this.items, element);
+  }
+
+  clear() {
+    this.items = {};
+  }
+
+  size() {
+    return Object.keys(this.items).length;
+  }
+
+  values() {
+    return Object.values(this.items);
+  }
+}
+
+module.exports = Set;
---
+++ estrutura-de-dados/sets/test.js
@@ -0,0 +1,9 @@
+const Set = require("./Set");
+const set = new Set();
+set.add("Rafa");
+set.add("Rafa");
+set.add("Raf");
+
+console.log(set.size());
+console.log(set.values());
+console.log(set);
